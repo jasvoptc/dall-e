8519ab07c83f8f7a69d10ceba165b58186c9bbf2
jasvo <jasvoptc@gmail.com>
2023-02-04 15:17:00 +0100

    5 commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -50,7 +50,7 @@ function CreatePost() {
       setLoading(true);
 
       try {
-        const response = await fetch ('http://localhost:8080/api/v1/post', {
+        const response = await fetch ('https://dall-e-2peg.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
